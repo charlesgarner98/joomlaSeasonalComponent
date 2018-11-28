5de3fb3d5cae9fd2bf691962dddbe3f23ef050e1
Moaxxe <charlesgarner98@gmail.com>
2018-11-28 16:41:38 +0000

    Added update 2.0.0

---
+++ component/sql/updates/mysql/2.0.0.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS `#__seasonal`;
+
+CREATE TABLE `#__seasonal` (
+	`id`       INT(11)     NOT NULL AUTO_INCREMENT,
+	`element` VARCHAR(25) NOT NULL,
+	`published` tinyint(4) NOT NULL DEFAULT '1',
+	PRIMARY KEY (`id`)
+)
+	ENGINE =MyISAM
+	AUTO_INCREMENT =0
+	DEFAULT CHARSET =utf8;
+
+INSERT INTO `#__seasonal` (`element`) VALUES
+('Snow'),
+('Music'),
+('Fireworks');
